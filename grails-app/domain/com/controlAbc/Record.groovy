0ce1a9745fe335045f5febb9900c6df08a3ee33d
cggg88jorge <jorge@makingdevs.com>
2015-06-15 23:11:35 -0500

    se agrega los contraints y se ajusta nombre de propiedad

--- grails-app/domain/com/controlAbc/Record.groovy
+++ grails-app/domain/com/controlAbc/Record.groovy
@@ -4,12 +4,18 @@ class Record {
 
   TypeChange change 
   String applicant
-  Date dateOfCompletion
+  Date dateOfFinished
   String executor
   String description
 
   Site site
 
   static constraints = {
+    change nullable: false
+    applicant nullable: false
+    dateOfFinished nullable: false
+    executor nullable: false
+    description nullable: false, range: 1..1000
+    site nullable: false
   }
 }
