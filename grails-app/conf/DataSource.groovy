7d85162795ba780cc3add27e309e7b5b53ca6a09
cggg88jorge <jorge@makingdevs.com>
2015-06-15 23:11:16 -0500

    se modifica el datasource por lo mientras despues se externalizara el archivo

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,31 @@
 dataSource {
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+  pooled = true
+  url = "jdbc:mysql://localhost:3306/control-abc"
+  driverClassName = "com.mysql.jdbc.Driver"
+  dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+  username = "root"
+  password = ""
+  properties {
+    jmxEnabled = true
+    initialSize = 5
+    maxActive = 50
+    minIdle = 5
+    maxIdle = 25
+    maxWait = 10000
+    maxAge = 10 * 60000
+    timeBetweenEvictionRunsMillis = 5000
+    minEvictableIdleTimeMillis = 60000
+    validationQuery = "SELECT 1"
+    validationQueryTimeout = 3
+    validationInterval = 15000
+    testOnBorrow = true
+    testWhileIdle = true
+    testOnReturn = false
+    jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+    defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+  } 
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -12,48 +33,4 @@ hibernate {
     cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
-}
-
-// environment specific settings
-environments {
-    development {
-        dataSource {
-            dbdir = "${System.getProperty('user.home')}/db/base"
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:file:${dbdir};MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    production {
-        dataSource {
-            dbdir = "${System.getProperty('user.home')}/db/base"
-            dbCreate = "update"
-            url = "jdbc:h2:file:${dbdir};MVCC=TRUE;LOCK_TIMEOUT=10000"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
-        }
-    }
 }
