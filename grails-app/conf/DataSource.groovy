a7cacc040cf7a9c64b1e3f4fee577e699fdaf600
cggg88jorge <jorge@makingdevs.com>
2015-06-21 16:08:01 -0500

    se externaliza el datasource se quita de este archivo

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,31 +1,3 @@
-dataSource {
-  pooled = true
-  url = "jdbc:mysql://localhost:3306/control-abc"
-  driverClassName = "com.mysql.jdbc.Driver"
-  dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-  username = "root"
-  password = ""
-  properties {
-    jmxEnabled = true
-    initialSize = 5
-    maxActive = 50
-    minIdle = 5
-    maxIdle = 25
-    maxWait = 10000
-    maxAge = 10 * 60000
-    timeBetweenEvictionRunsMillis = 5000
-    minEvictableIdleTimeMillis = 60000
-    validationQuery = "SELECT 1"
-    validationQueryTimeout = 3
-    validationInterval = 15000
-    testOnBorrow = true
-    testWhileIdle = true
-    testOnReturn = false
-    jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
-    defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-  } 
-}
-
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
