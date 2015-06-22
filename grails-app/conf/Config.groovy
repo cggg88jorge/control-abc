5a14bfc8b76ed4fd697ec7accb2599b7e37bca92
cggg88jorge <jorge@makingdevs.com>
2015-06-21 19:43:33 -0500

    se agrega configuracion de migration para hacer el updatae automatico cada que levant al aplicacion

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -78,6 +78,9 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+grails.plugin.databasemigration.updateOnStart = true
+grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
+
 environments {
     development {
         grails.logging.jul.usebridge = true
