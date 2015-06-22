9b8d75c2b9c0369e0129f43487dec1e04cff9015
cggg88jorge <jorge@makingdevs.com>
2015-06-21 19:05:43 -0500

    se crea nuevamente el changelog ya que se modifico clases

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,28 +1,16 @@
 databaseChangeLog = {
 
-	changeSet(author: "cggg88jorge (generated)", id: "1434426248320-1") {
-		createTable(tableName: "record") {
+	changeSet(author: "cggg88jorge (generated)", id: "1434926605591-1") {
+		createTable(tableName: "incidence") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "recordPK")
+				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "incidencePK")
 			}
 
 			column(name: "version", type: "bigint") {
 				constraints(nullable: "false")
 			}
 
-			column(name: "applicant", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "change", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "date_of_finished", type: "datetime") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "description", type: "varchar(255)") {
+			column(name: "description", type: "varchar(1000)") {
 				constraints(nullable: "false")
 			}
 
@@ -30,13 +18,25 @@ databaseChangeLog = {
 				constraints(nullable: "false")
 			}
 
+			column(name: "finished_date", type: "datetime") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "requested", type: "varchar(255)") {
+				constraints(nullable: "false")
+			}
+
 			column(name: "site_id", type: "bigint") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "type_change", type: "varchar(255)") {
 				constraints(nullable: "false")
 			}
 		}
 	}
 
-	changeSet(author: "cggg88jorge (generated)", id: "1434426248320-2") {
+	changeSet(author: "cggg88jorge (generated)", id: "1434926605591-2") {
 		createTable(tableName: "site") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "sitePK")
@@ -56,19 +56,19 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "cggg88jorge (generated)", id: "1434426248320-4") {
-		createIndex(indexName: "FK_fq98ett8cbgv7246xck23jsvo", tableName: "record") {
+	changeSet(author: "cggg88jorge (generated)", id: "1434926605591-4") {
+		createIndex(indexName: "FK_hoq6nieeyp7t12q7a5vvtdjv0", tableName: "incidence") {
 			column(name: "site_id")
 		}
 	}
 
-	changeSet(author: "cggg88jorge (generated)", id: "1434426248320-5") {
-		createIndex(indexName: "cr_uniq_1434426248251", tableName: "site", unique: "true") {
+	changeSet(author: "cggg88jorge (generated)", id: "1434926605591-5") {
+		createIndex(indexName: "cr_uniq_1434926605559", tableName: "site", unique: "true") {
 			column(name: "cr")
 		}
 	}
 
-	changeSet(author: "cggg88jorge (generated)", id: "1434426248320-3") {
-		addForeignKeyConstraint(baseColumnNames: "site_id", baseTableName: "record", constraintName: "FK_fq98ett8cbgv7246xck23jsvo", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "site", referencesUniqueColumn: "false")
+	changeSet(author: "cggg88jorge (generated)", id: "1434926605591-3") {
+		addForeignKeyConstraint(baseColumnNames: "site_id", baseTableName: "incidence", constraintName: "FK_hoq6nieeyp7t12q7a5vvtdjv0", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "site", referencesUniqueColumn: "false")
 	}
 }
